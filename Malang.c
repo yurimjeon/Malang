4596658e864f8087e7130e51111038cc9647caad
yurimjeon <kkbkkasis@gmail.com>
2015-11-05 08:23:13 -0800

    UI_etc

--- Malang.c
+++ Malang.c
@@ -3,7 +3,12 @@
 
 GtkWidget *window;
 GtkWidget *vbox;
-GtkWidget *label;
+GtkWidget *hbox;
+GtkWidget *label1,*label2,*label3;
+
+char buf[100];
+int numLevel = 0;
+int numHeart = 0;
 
 
 int main(int argc, char *argv[])
@@ -13,12 +18,30 @@ int main(int argc, char *argv[])
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-	vbox = gtk_vbox_new(TRUE,0);
-	label = gtk_label_new("Malang start");
-	gtk_widget_set_size_request(label,100,50);
+	hbox = gtk_hbox_new(TRUE,0);
+	gtk_widget_set_size_request(hbox,300,60);
+	label1 = gtk_label_new("Level");
+	gtk_widget_set_size_request(label1,100,20);
 
-	gtk_container_add(GTK_CONTAINER(vbox),label);
-	gtk_container_add(GTK_CONTAINER(window), vbox);
+	label2 =gtk_label_new("Correct!");
+	gtk_widget_set_size_request(label2,100,20);
+
+	label3 =gtk_label_new("Life");
+	gtk_widget_set_size_request(label2,100,20);
+
+
+	gtk_container_add(GTK_CONTAINER(hbox),label1);
+	gtk_container_add(GTK_CONTAINER(hbox),label2);
+	gtk_container_add(GTK_CONTAINER(hbox),label3);
+
+	//button
+
+
+	vbox = gtk_vbox_new(TRUE,0);
+        gtk_widget_set_size_request(vbox,300,500);
+
+	//
+	gtk_container_add(GTK_CONTAINER(window), hbox);
 
 	gtk_widget_show_all(window);
 
